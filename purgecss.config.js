a7e7110484e20639e441617a4e1d4a7ae1fdd3c4
Pablo Rodriguez Mier <pablo.rodriguez.mier@gmail.com>
2026-05-31 16:16:05 +0200

    Safelist Jupyter CSS during purge

--- purgecss.config.js
+++ purgecss.config.js
@@ -2,5 +2,17 @@ module.exports = {
   content: ["_site/**/*.html", "_site/**/*.js"],
   css: ["_site/assets/css/*.css"],
   output: "_site/assets/css/",
+  safelist: {
+    standard: [
+      /^jp-/,
+      /^lm-/,
+      /^cm-/,
+      /^CodeMirror/,
+      /^highlight/,
+      /^MathJax/,
+      /^MJX/,
+      /^mermaid/,
+    ],
+  },
   skippedContentGlobs: ["_site/assets/**/*.html"],
 };
